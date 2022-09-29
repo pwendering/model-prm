5f69ad1d036ad9c0132fa147bb6f6d188311fac3
pwendering <philipp.wendering@gmail.com>
2022-09-29 08:55:57 +0200

    Remove o/c ratio from analysis

--- Code/model_phot_resp_tfa_oc_ratio.m
+++ Code/model_phot_resp_tfa_oc_ratio.m
@@ -228,14 +228,11 @@ solFBA = optimizeCbModel(model);
 min_obj = roundsd(0.4*solFBA.f, 2, 'floor');
 model.lb(model.c==1) = min_obj;
 
-%% Perform FVA at 90% of the optimum
+%% Perform FVA at 90% of the optimum (without oxygenation to carboxylation ratio)
 old_ub = model.lb(model.c==1);
 model.lb(model.c==1) = 0.9*solFBA.f;
 fva_wt = runMinMax(model,model.rxns,'runParallel',PAR_FLAG);
 model.lb(model.c==1) = old_ub;
-% - logical vector with indices for bidirectional reactions (flux ranges crossing zero)
-n = @(x) x(:,1)<-1e-9 & x(:,2)>1e-9;
-is_bd_fva_wt = (n(fva_wt));
 
 % Are there any blocked reactions?
 % solver tolerance is 1e-9
@@ -263,11 +260,48 @@ tmp = convToTFA(prepped_m, ReactionDB, [], 'DGo', [], min_obj);
 this_tmodel = addNetFluxVariables(tmp);
 NF_idx = getAllVar(this_tmodel,{'NF'});
 
-%% Perform FVA at 90% of the optimum
+%% Constrain oxygenation to carboxylation ratio
+s_co = [
+    92 107 89 105,... % Parry et al. (1989) [https://doi.org/10.1093/jxb/40.3.317]
+    82 74 89 93 61 66,... % Zhu et al. (1992) [https://doi.org/10.1104%2Fpp.98.2.764]
+    99.9 100.8 96.2 96 98.7 98.4 97 99.2 98.5 94 100.8 95.6 93.1 99.7,...
+    92.4 95.4 97.0 100.1 97.5 82.2 87.3 84.4]; % Hermida-Carrera et al. (2016) [https://doi.org/10.1104%2Fpp.16.01846]
+f_mol_bar = 2417.20 / 104.90; % Walker et al. (2013) [https://doi.org/10.1111/pce.12166]
+O = 210000;  % Farquhar et al. (1980), Planta
+C = 230;  % Farquhar et al. (1980), Planta
+phi = 0.27; % Farquhar et al. (1980), Planta
+phi_tol = 0.1*phi; % allow 10% variation
+
+% phi_array = (1 ./ (f_mol_bar*s_co))*(O/C);
+% phi = mean(phi_array);
+% phi_tol = 2 * std(phi_array);
+
+model = addMetabolite(model, 'phi_lb', 'csense', 'L');
+model.S(findMetIDs(model, 'phi_lb'), findRxnIDs(model, {'RBC_h' 'RBO_h'})) = [phi-phi_tol -1];
+
+model = addMetabolite(model, 'phi_ub', 'csense', 'L');
+model.S(findMetIDs(model, 'phi_ub'), findRxnIDs(model, {'RBC_h' 'RBO_h'})) = [-phi-phi_tol 1];
+
+this_tmodel.constraintNames(end+1) = {'phi_lb'};
+this_tmodel.constraintType(end+1) = {'<'};
+this_tmodel.rhs(end+1) = 0;
+this_tmodel.A = [this_tmodel.A; zeros(1, size(this_tmodel.A,2))];
+this_tmodel.A(end, cellfun(@(x)find(ismember(this_tmodel.varNames, x)), {'NF_RBC_h' 'NF_RBO_h'})) = [phi-phi_tol -1];
+
+this_tmodel.constraintNames(end+1) = {'phi_ub'};
+this_tmodel.constraintType(end+1) = {'<'};
+this_tmodel.rhs(end+1) = 0;
+this_tmodel.A = [this_tmodel.A; zeros(1, size(this_tmodel.A,2))];
+this_tmodel.A(end, cellfun(@(x)find(ismember(this_tmodel.varNames, x)), {'NF_RBC_h' 'NF_RBO_h'})) = [-phi-phi_tol 1];
+
+%% Perform FVA at 90% of the optimum (with oxygenation to carboxylation ratio)
 old_ub = model.lb(model.c==1);
 model.lb(model.c==1) = 0.9*solFBA.f;
 fva_wt = runMinMax(model,model.rxns,'runParallel',PAR_FLAG);
 model.lb(model.c==1) = old_ub;
+% - logical vector with indices for bidirectional reactions (flux ranges crossing zero)
+n = @(x) x(:,1)<-1e-9 & x(:,2)>1e-9;
+is_bd_fva_wt = (n(fva_wt));
 
 %% Read experimental data
 data_dir_struct = dir(data_dir);
