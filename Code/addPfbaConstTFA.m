236c020dcbf04b769cbb8b1052753122fe383ffe
pwendering <philipp.wendering@gmail.com>
2022-10-12 11:34:53 +0200

    Allow for custom RHA vector to allow use for min between wt and mutant

--- Code/addPfbaConstTFA.m
+++ Code/addPfbaConstTFA.m
@@ -1,4 +1,4 @@
-function pfba_model = addPfbaConstTFA(model)
+function pfba_model = addPfbaConstTFA(model, rhsVector)
 %% pfba_model = addPfbaConstTFA(model)
 % This function adds constraints to a TFA problem to enable minimization of
 % absolute net fluxes. This is done by decomposing reaction net flux into a
@@ -14,26 +14,32 @@ function pfba_model = addPfbaConstTFA(model)
 nf_idx = startswith(model.varNames, 'NF_');
 n_nf = sum(nf_idx);
 
+if nargin < 2 || isempty(rhsVector)
+    rhsVector = zeros(n_nf, 1);
+elseif numel(rhsVector) ~= n_nf
+    error('Size of provided RHS vector does not match the number of NF variables')
+end
+
 % construct additional matrix with delta variables to be appended to the
 % existing constraint matrix
 delta_mat = [zeros(n_nf, size(model.A,2)) -eye(n_nf) eye(n_nf)];
 % fill in ones for NF reaction indices
-delta_mat(1:n_nf, nf_idx) = 1;
+delta_mat(:, nf_idx) = 1;
 
 % append new matrix to existing matrix of TFA problem
 pfba_model = model;
 pfba_model.A = [[pfba_model.A zeros(size(pfba_model.A, 1), 2*n_nf)]; delta_mat];
 
 % add additional information for constraints and variables
-pfba_model.rhs = [pfba_model.rhs; zeros(n_nf, 1)];
+pfba_model.rhs = [pfba_model.rhs; rhsVector];
 pfba_model.constraintNames = [pfba_model.constraintNames; strcat(model.varNames(nf_idx), '_pfba')];
-pfba_model.constraintType = [pfba_model.constraintType; repelem('=', n_nf, 1)];
+pfba_model.constraintType = [pfba_model.constraintType; repelem({'='}, n_nf, 1)];
 
 pfba_model.varNames = [pfba_model.varNames; strcat(pfba_model.varNames(nf_idx), '_delta_plus');...
     strcat(pfba_model.varNames(nf_idx), '_delta_minus')];
 pfba_model.var_lb = [pfba_model.var_lb; zeros(2*n_nf, 1)];
 pfba_model.var_ub = [pfba_model.var_ub; repelem(1000, 2*n_nf, 1)];
-
+pfba_model.vartypes = [pfba_model.vartypes; repmat({'C'}, 2*n_nf, 1)];
 % set objective to minimization of deltas
 pfba_model.f = [zeros(size(model.f)) ones(2*n_nf, 1)];
 
