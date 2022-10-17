c34c73fe7b4a1e7e27cb0faba04b1532f03fb788
pwendering <philipp.wendering@gmail.com>
2022-10-17 18:02:24 +0200

    Multiple improvements; set UB for NF reactions to 1000

--- Code/model_phot_resp_tfa_oc_ratio_pfba_min_dist_mut_wt.m
+++ Code/model_phot_resp_tfa_oc_ratio_pfba_min_dist_mut_wt.m
@@ -261,7 +261,7 @@ tmp_model.S(findMetIDs(tmp_model, 'phi_ub'), findRxnIDs(tmp_model, {'RBC_h' 'RBO
 %% Test simple fba
 solFBA = optimizeCbModel(tmp_model);
 % We can set a lower bound for growth (e.g. 50% of maximal growth)
-min_obj = roundsd(0.4*solFBA.f, 2, 'floor');
+min_obj = roundsd(0.5*solFBA.f, 2, 'floor');
 tmp_model.lb(tmp_model.c==1) = min_obj;
 
 %% Perform FVA at 90% of the optimum
@@ -301,6 +301,9 @@ tmp = convToTFA(prepped_m, ReactionDB, [], 'NO', [], min_obj);
 % NF_rxn = F_rxn - B_rxn
 this_tmodel = addNetFluxVariables(tmp);
 NF_idx = getAllVar(this_tmodel,{'NF'});
+
+% since this function sets all upper bounds of NF reactions to 100, set them to 1000
+this_tmodel.var_ub(NF_idx) = 1000;
 
 this_tmodel.constraintNames(end+1) = {'phi_lb'};
 this_tmodel.constraintType(end+1) = {'<'};
@@ -554,138 +557,140 @@ for lc_idx = 1:numel(l_cond)
             %% Solve TFA for wild type model
             wt_tmodel = this_tmodel;
             
-            % if calculated growth rate for FL applies, constrain ratio
-            % of growth rate between ML and FL
-            if ismember(l_cond(lc_idx), 'fl') || ismember(l_cond(lc_idx), 'fl_ml')
-                wt_tmodel.var_ub(wt_tmodel.f==1) = ref_ml_growth * mu_wt_fl / mu_wt_ml + 1e-10;
-                wt_tmodel.var_lb(wt_tmodel.f==1) = ref_ml_growth * mu_wt_fl / mu_wt_ml - 1e-10;
+            if m == 1
+                % if calculated growth rate for FL applies, constrain ratio
+                % of growth rate between ML and FL
+                if ismember(l_cond(lc_idx), 'fl') || ismember(l_cond(lc_idx), 'fl_ml')
+                    wt_tmodel.var_ub(wt_tmodel.f==1) = ref_ml_growth * mu_wt_fl / mu_wt_ml + 1e-10;
+                    wt_tmodel.var_lb(wt_tmodel.f==1) = ref_ml_growth * mu_wt_fl / mu_wt_ml - 1e-10;
+                    
+                    phi = phi_fl;
+                    phi_tol = phi_tol_fl;
+                else
+                    phi = phi_ml;
+                    phi_tol = phi_tol_ml;
+                end
                 
-                phi = phi_fl;
-                phi_tol = phi_tol_fl;
-            else
-                phi = phi_ml;
-                phi_tol = phi_tol_ml;
-            end
-            
-            % add oxygenation to carboxylation ratio
-            rbc_rxn_idx = cellfun(@(x)find(ismember(wt_tmodel.varNames, x)), {'NF_RBC_h' 'NF_RBO_h'});
-            
-            phi_lb_constr_idx = contains(wt_tmodel.constraintNames, 'phi_lb');
-            wt_tmodel.A(phi_lb_constr_idx, rbc_rxn_idx) = [phi-phi_tol -1];
-            
-            phi_lb_constr_idx = contains(wt_tmodel.constraintNames, 'phi_ub');
-            wt_tmodel.A(phi_lb_constr_idx, rbc_rxn_idx) = [-phi-phi_tol 1];
-            
-            % solve wt model to get maximum predicted growth rate
-            bio_obj = wt_tmodel.f;
-            tfa_wt = solveTFAmodelCplex(wt_tmodel);
-            wt_tmodel.var_lb(wt_tmodel.f==1) = 0.99*tfa_wt.x(bio_obj==1);
-            
-            % add relaxed metabolite concentration ranges to measured
-            % metabolites
-            wt_tmodel = addRelaxedMetConcRanges(wt_tmodel,LC_varNames,log(C_lb_wt),log(C_ub_wt));
-            
-            % add minimization objective for relaxation variables
-            wt_tmodel.f(startsWith(wt_tmodel.varNames,'EPS_')) = -1e-3;
-            
-            % solve TFA
-            tfa_wt = solveTFAmodelCplex(wt_tmodel);
-            
-            % reset model objective
-            wt_tmodel.f(:) = 0;
-            wt_tmodel.f(bio_obj==1) = 1;
-            
-            if isempty(tfa_wt.x)
-                fprintf('WT thermo model cannot be solved with relaxed metabolite concentration ranges\n')
-                continue
-            end
-            
-            % positive and negative concentration relaxations
-            eps_minus_wt = tfa_wt.x(startsWith(wt_tmodel.varNames,'EPS_MINUS'));
-            eps_plus_wt = tfa_wt.x(startsWith(wt_tmodel.varNames,'EPS_PLUS'));
-            ch_conc_idx = eps_minus_wt>1e-4 | eps_plus_wt>1e-4;
-            
-            % (relaxed) TFA metabolite concentrations
-            met_conc_wt = tfa_wt.x(cellfun(@(x)find(ismember(wt_tmodel.varNames,x)),...
-                LC_varNames));
-            
-            % create figure with updated metabolite concentration ranges
-            tmp_fig = figure(...
-                'Visible','off',...
-                'units','normalized',...
-                'outerposition',[0 0 1 1]);
-            tiledlayout(2,1)
-            nexttile
-            labels = categorical(strrep(erase(LC_varNames,'LC_'),'_','\_'));
-            arrayfun(@(i)line([labels(i) labels(i)],[C_lb_wt(i) C_ub_wt(i)],'linewidth',4,'color','k'),1:numel(LC_varNames))
-            hold on
-            h = zeros(2,1);
-            h(1)=scatter(labels(~ch_conc_idx),exp(met_conc_wt(~ch_conc_idx)),15,'filled');
-            h(2)=scatter(labels(ch_conc_idx),exp(met_conc_wt(ch_conc_idx)),15,'filled');
-            hold off
-            set(gca,'YScale','log','FontSize',10)
-            legend(h,{'unchanged','relaxed'},'FontSize',14,'box','off',...
-                'location','southeast')
-            ylabel('TFA metabolite concentration [M]','FontSize',14)
-            text(0.01,0.98,'Col-0','units','normalized','fontweight','bold')
-            
-            % fix metabolite measured metabolite concentrations to newly
-            % obtained values with 10% tolerance
-            wt_tmodel.var_lb(cellfun(@(x)find(ismember(wt_tmodel.varNames,x)),...
-                LC_varNames)) = met_conc_wt-abs(.1*(met_conc_wt));
-            wt_tmodel.var_ub(cellfun(@(x)find(ismember(wt_tmodel.varNames,x)),...
-                LC_varNames)) = met_conc_wt+abs(.1*(met_conc_wt));
-            
-            % WT optimal growth rate
-            tfa_wt_2 = solveTFAmodelCplex(wt_tmodel);
-            if ~isempty(tfa_wt_2.x)
-                wt_opt = tfa_wt_2.x(wt_tmodel.f==1);
-            else
-                fprintf('Growth optimization with fixed metabolite concentrations not successful\n')
-                wt_opt = tfa_wt.x(wt_tmodel.f==1);
-            end
-            
-            % save optimal growth rate for ML condition
-            if lc_idx==1
-                ref_ml_growth = wt_opt;
-            end
-            
-            % minimize the sum of absolute net fluxes at optimal
-            % biomass predicted by TFA
-            wt_tmodel.var_lb(wt_tmodel.f==1) = 0.99*wt_opt;
-            
-            % add pFBA constraints to TFA problem
-            wt_tmodel_pfba = addPfbaConstTFA(wt_tmodel);
-            
-            % solve pFBA problem
-            wt_pfba_sol = solveTFAmodelCplex(wt_tmodel_pfba);
-            wt_pfba_obj_val = sum(wt_pfba_sol.x(wt_tmodel_pfba.f==1));
-            wt_pfba_flux = wt_pfba_sol.x;
-            
-            fprintf('WT pFBA optimal value: %.4g\n', wt_pfba_obj_val)
-            
-            % fix the optimal (minimal) sum of fluxes before sampling
-            CLHS.varIDs = 1:size(wt_tmodel_pfba.A, 2);
-            CLHS.varCoeffs = [zeros(1, size(wt_tmodel.A, 2)) ...
-                ones(1, size(wt_tmodel_pfba.A, 2) - size(wt_tmodel.A, 2))];
-            wt_tmodel_pfba = addNewConstraintInTFA(wt_tmodel_pfba, 'sum_pFBA', '<',...
-                CLHS, 1.01*wt_pfba_obj_val);
-            
-            % set lower bound for biomass to 90% of optimal value
-            wt_tmodel_pfba.var_lb(bio_obj==1) = 0.9*wt_opt;
-            
-            % Run tva with the data
-            fprintf('Running variability analysis\n')
-            tva_wt = runTMinMax(wt_tmodel_pfba, wt_tmodel_pfba.varNames(NF_idx),...
-                'runParallel',PAR_FLAG);
-            
-            % Run flux sampling
-            if m_idx == 1
-                fprintf('Running sampling\n')
-                tic
-                [fluxSamples_wt,concSamples_wt] = sampleTModel(wt_tmodel_pfba,tva_wt(:,1),tva_wt(:,2),N_SAMPLES,N_CPU);
-                t_end = toc;
-                fprintf('Sampling time: %.2f s\n',t_end)
+                % add oxygenation to carboxylation ratio
+                rbc_rxn_idx = cellfun(@(x)find(ismember(wt_tmodel.varNames, x)), {'NF_RBC_h' 'NF_RBO_h'});
+                
+                phi_lb_constr_idx = contains(wt_tmodel.constraintNames, 'phi_lb');
+                wt_tmodel.A(phi_lb_constr_idx, rbc_rxn_idx) = [phi-phi_tol -1];
+                
+                phi_lb_constr_idx = contains(wt_tmodel.constraintNames, 'phi_ub');
+                wt_tmodel.A(phi_lb_constr_idx, rbc_rxn_idx) = [-phi-phi_tol 1];
+                
+                % solve wt model to get maximum predicted growth rate
+                bio_obj = wt_tmodel.f;
+                tfa_wt = solveTFAmodelCplex(wt_tmodel);
+                wt_tmodel.var_lb(wt_tmodel.f==1) = 0.99*tfa_wt.x(bio_obj==1);
+                
+                % add relaxed metabolite concentration ranges to measured
+                % metabolites
+                wt_tmodel = addRelaxedMetConcRanges(wt_tmodel,LC_varNames,log(C_lb_wt),log(C_ub_wt));
+                
+                % add minimization objective for relaxation variables
+                wt_tmodel.f(startsWith(wt_tmodel.varNames,'EPS_')) = -1e-3;
+                
+                % solve TFA
+                tfa_wt = solveTFAmodelCplex(wt_tmodel);
+                
+                % reset model objective
+                wt_tmodel.f(:) = 0;
+                wt_tmodel.f(bio_obj==1) = 1;
+                
+                if isempty(tfa_wt.x)
+                    fprintf('WT thermo model cannot be solved with relaxed metabolite concentration ranges\n')
+                    continue
+                end
+                
+                % positive and negative concentration relaxations
+                eps_minus_wt = tfa_wt.x(startsWith(wt_tmodel.varNames,'EPS_MINUS'));
+                eps_plus_wt = tfa_wt.x(startsWith(wt_tmodel.varNames,'EPS_PLUS'));
+                ch_conc_idx = eps_minus_wt>1e-4 | eps_plus_wt>1e-4;
+                
+                % (relaxed) TFA metabolite concentrations
+                met_conc_wt = tfa_wt.x(cellfun(@(x)find(ismember(wt_tmodel.varNames,x)),...
+                    LC_varNames));
+                
+                % create figure with updated metabolite concentration ranges
+                tmp_fig = figure(...
+                    'Visible','off',...
+                    'units','normalized',...
+                    'outerposition',[0 0 1 1]);
+                tiledlayout(2,1)
+                nexttile
+                labels = categorical(strrep(erase(LC_varNames,'LC_'),'_','\_'));
+                arrayfun(@(i)line([labels(i) labels(i)],[C_lb_wt(i) C_ub_wt(i)],'linewidth',4,'color','k'),1:numel(LC_varNames))
+                hold on
+                h = zeros(2,1);
+                h(1)=scatter(labels(~ch_conc_idx),exp(met_conc_wt(~ch_conc_idx)),15,'filled');
+                h(2)=scatter(labels(ch_conc_idx),exp(met_conc_wt(ch_conc_idx)),15,'filled');
+                hold off
+                set(gca,'YScale','log','FontSize',10)
+                legend(h,{'unchanged','relaxed'},'FontSize',14,'box','off',...
+                    'location','southeast')
+                ylabel('TFA metabolite concentration [M]','FontSize',14)
+                text(0.01,0.98,'Col-0','units','normalized','fontweight','bold')
+                
+                % fix metabolite measured metabolite concentrations to newly
+                % obtained values with 10% tolerance
+                wt_tmodel.var_lb(cellfun(@(x)find(ismember(wt_tmodel.varNames,x)),...
+                    LC_varNames)) = met_conc_wt-abs(.1*(met_conc_wt));
+                wt_tmodel.var_ub(cellfun(@(x)find(ismember(wt_tmodel.varNames,x)),...
+                    LC_varNames)) = met_conc_wt+abs(.1*(met_conc_wt));
+                
+                % WT optimal growth rate
+                tfa_wt_2 = solveTFAmodelCplex(wt_tmodel);
+                if ~isempty(tfa_wt_2.x)
+                    wt_opt = tfa_wt_2.x(wt_tmodel.f==1);
+                else
+                    fprintf('Growth optimization with fixed metabolite concentrations not successful\n')
+                    wt_opt = tfa_wt.x(wt_tmodel.f==1);
+                end
+                
+                % save optimal growth rate for ML condition
+                if lc_idx==1
+                    ref_ml_growth = wt_opt;
+                end
+                
+                % minimize the sum of absolute net fluxes at optimal
+                % biomass predicted by TFA
+                wt_tmodel.var_lb(wt_tmodel.f==1) = 0.99*wt_opt;
+                
+                % add pFBA constraints to TFA problem
+                wt_tmodel_pfba = addPfbaConstTFA(wt_tmodel);
+                
+                % solve pFBA problem
+                wt_pfba_sol = solveTFAmodelCplex(wt_tmodel_pfba);
+                wt_pfba_obj_val = sum(wt_pfba_sol.x(wt_tmodel_pfba.f==1));
+                wt_pfba_flux = wt_pfba_sol.x;
+                
+                fprintf('WT pFBA optimal value: %.4g\n', wt_pfba_obj_val)
+                
+                % fix the optimal (minimal) sum of fluxes before sampling
+                CLHS.varIDs = 1:size(wt_tmodel_pfba.A, 2);
+                CLHS.varCoeffs = [zeros(1, size(wt_tmodel.A, 2)) ...
+                    ones(1, size(wt_tmodel_pfba.A, 2) - size(wt_tmodel.A, 2))];
+                wt_tmodel_pfba = addNewConstraintInTFA(wt_tmodel_pfba, 'sum_pFBA', '<',...
+                    CLHS, 1.01*wt_pfba_obj_val);
+                
+                % set lower bound for biomass to 90% of optimal value
+                wt_tmodel_pfba.var_lb(bio_obj==1) = 0.9*wt_opt;
+                
+                % Run tva with the data
+                fprintf('Running variability analysis\n')
+                tva_wt = runTMinMax(wt_tmodel_pfba, wt_tmodel_pfba.varNames(NF_idx),...
+                    'runParallel',PAR_FLAG);
+                
+                % Run flux sampling
+                if m_idx == 1
+                    fprintf('Running sampling\n')
+                    tic
+                    [fluxSamples_wt,concSamples_wt] = sampleTModel(wt_tmodel_pfba,tva_wt(:,1),tva_wt(:,2),N_SAMPLES,N_CPU);
+                    t_end = toc;
+                    fprintf('Sampling time: %.2f s\n',t_end)
+                end
             end
             
             %% solve TFA for mutant model
@@ -704,7 +709,7 @@ for lc_idx = 1:numel(l_cond)
             clear mut_model mutFBASol
             
             % add oxygenation to carboxylation ratio
-            rbc_rxn_idx = cellfun(@(x)find(ismember(wt_tmodel.varNames, x)), {'F_RBC_h' 'F_RBO_h'});
+            rbc_rxn_idx = cellfun(@(x)find(ismember(wt_tmodel.varNames, x)), {'NF_RBC_h' 'NF_RBO_h'});
             
             phi_lb_constr_idx = contains(wt_tmodel.constraintNames, 'phi_lb');
             wt_tmodel.A(phi_lb_constr_idx, rbc_rxn_idx) = [phi-phi_tol -1];
@@ -759,7 +764,8 @@ for lc_idx = 1:numel(l_cond)
                 'location','southeast')
             ylabel('TFA metabolite concentration [M]','FontSize',14)
             text(0.01,0.98,['{\it ' mutants{m_idx} '}'],'units','normalized','fontweight','bold')
-            
+            set(gcf, 'units', 'normalized', 'outerposition', [0 0 1 1])
+            pause(5)
             exportgraphics(tmp_fig,[res_dir filesep 'metabolite_concentration_' mutants{m_idx} ...
                 '_' l_cond{lc_idx} '_timepoint_' num2str(tp(t_idx)) '_phUB_' num2str(ph_ub,3) '.png'])
             delete(tmp_fig)
