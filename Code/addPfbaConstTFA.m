b5b0e8800f7b9a6d5d72973d3c366b62d92955c6
pwendering <philipp.wendering@gmail.com>
2022-10-18 12:13:20 +0200

    Exlude NF of objective, assuming that F variable is optimized

--- Code/addPfbaConstTFA.m
+++ Code/addPfbaConstTFA.m
@@ -13,7 +13,7 @@ function pfba_model = addPfbaConstTFA(model, rhsVector)
 % find net flux variables
 nf_idx = startsWith(model.varNames, 'NF_');
 % exlude biomass reaction
-nf_idx(model.f==1) = 0;
+nf_idx(ismember(model.varNames, strcat('N',model.varNames(model.f==1)))) = 0;
 n_nf = sum(nf_idx);
 
 if nargin < 2 || isempty(rhsVector)
