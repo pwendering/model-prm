762bfca666f8a5d27a6c637dfad96ea8cc91f7e3
pwendering <philipp.wendering@gmail.com>
2022-11-28 10:45:50 +0200

    Add documentation

--- Code/createRandomFluxVector.m
+++ Code/createRandomFluxVector.m
@@ -1,4 +1,13 @@
-function v = createRandomFluxVector(minFlux,maxFlux)
-        l = numel(minFlux);
-        v = (maxFlux-minFlux) .* rand(l,1) + minFlux;
+function v = createRandomFluxVector(minFlux, maxFlux)
+%% v = createRandomFluxVector(minFlux, maxFlux)
+% Creates a randomvector with between minimal and maximal concentrations
+% for each element in minFlux/maxFlux.
+% INPUT
+%   double minFlux:         array of minimum metabolite concentrations
+%   double maxFlux:         array of maximum metabolite concentrations
+% OUTPUT
+%   double v:               array of random concentrations within the
+%                           limits of minFlux and maxFlux for every element
+l = numel(minFlux);
+v = (maxFlux-minFlux) .* rand(l,1) + minFlux;
 end
