2e44c4ba30ef8310f8892abcad3daf4f4ba39bde
pwendering <philipp.wendering@gmail.com>
2022-11-28 11:01:20 +0200

    Add documentation

--- Code/performProjectionCPLEX.m
+++ Code/performProjectionCPLEX.m
@@ -1,5 +1,23 @@
-function projSol = performProjectionCPLEX(tModel,v)
-norm1_const_idx = startsWith(tModel.constraintNames,'norm1_NF_');
-tModel.rhs(norm1_const_idx) = v;
-projSol = solveTFAmodelCplex(tModel,30);
+function projSol = performProjectionCPLEX(model, v)
+% This function updates the right-hand side of constraints that specify the
+% minimization of absolute distances between the predicted and a random
+% flux vector. These constraints have the prefix 'norm_NF_' for each net flux
+% reaction in the TFA model.
+% INPUT
+%   struct model:           TFA model created with the matTFA toolbox;
+%                           contains constraints for the minimization of
+%                           the first norm of fluxes through net flux
+%                           reactions.
+%   double v:               vector of random flux values within ranges
+%                           determined by flux variability analysis with
+%                           thermodynamic constraints.
+% OUTPUT
+%   struct projSol:         solution structure
+
+% find minimization constraints
+norm1_const_idx = startsWith(model.constraintNames,'norm1_NF_');
+% add RHS values
+model.rhs(norm1_const_idx) = v;
+% solve problem
+projSol = solveTFAmodelCplex(model,30);
 end
